cf6e23a76ce893e77d5b9866b3bca0f71c7dd30b
Pertti Kroger <pertti@qvik.fi>
2020-01-09 15:46:31 +0200

    Handle "intent" and "android-app" schemes.

--- sdk/src/main/java/com/swedbankpay/mobilesdk/internal/WebViewFragment.kt
+++ sdk/src/main/java/com/swedbankpay/mobilesdk/internal/WebViewFragment.kt
@@ -2,6 +2,8 @@ package com.swedbankpay.mobilesdk.internal
 
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
@@ -16,14 +18,23 @@ import android.view.ViewGroup
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.swedbankpay.mobilesdk.R
+import java.net.URISyntaxException
 
 internal class WebViewFragment : Fragment() {
     private companion object {
         private const val STATE_WEBVIEW = "W"
         private const val STATE_BASE_HTML = "B"
+
+        private val INTENT_URI_FLAGS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            Intent.URI_ANDROID_APP_SCHEME or Intent.URI_INTENT_SCHEME
+        } else {
+            Intent.URI_INTENT_SCHEME
+        }
     }
 
     private var webView: WebView? = null
@@ -144,6 +155,10 @@ internal class WebViewFragment : Fragment() {
         return true
     }
 
+    private fun showIntentUriErrorDialog(uri: Uri) {
+        ErrorDialogFragment.newInstance(uri).show(childFragmentManager, null)
+    }
+
     private inner class MyWebViewClient : WebViewClient() {
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
             if (url != currentBaseUrl) {
@@ -164,15 +179,63 @@ internal class WebViewFragment : Fragment() {
         }
 
         private fun shouldOverrideUrlLoading(uri: Uri?): Boolean {
-            return uri != null
-                    && (attemptHandleByCallback(uri) || attemptHandleByExternalApp(uri))
+            return uri != null && (
+                    attemptHandleByViewModel(uri)
+                            || attemptHandleIntentUri(uri)
+                            || attemptHandleByExternalApp(uri)
+                    )
         }
 
-        private fun attemptHandleByCallback(uri: Uri): Boolean {
+        private fun attemptHandleByViewModel(uri: Uri): Boolean {
             val parentFragment = parentFragment ?: return false
             val context = context ?: return false
             val vm = ViewModelProviders.of(parentFragment)[InternalPaymentViewModel::class.java]
             return vm.overrideNavigation(context, uri)
+        }
+
+        private fun attemptHandleIntentUri(uri: Uri): Boolean {
+            val scheme = uri.scheme
+            if (scheme != "intent" && scheme != "android-app") return false
+
+            val success = try {
+                val intent = getIntentUriIntent(uri)
+                intent?.let(::startActivity)
+                intent != null
+            } catch (_: Exception) {
+                false
+            }
+            if (!success) {
+                showIntentUriErrorDialog(uri)
+            }
+
+            // Always intercept "intent" or "android-app" uris,
+            // as the WebView would choke on them anyway.
+            return true
+        }
+
+        private fun getIntentUriIntent(uri: Uri): Intent? {
+            val intent = try {
+                Intent.parseUri(uri.toString(), INTENT_URI_FLAGS)
+            } catch (_: URISyntaxException) {
+                null
+            }
+            intent?.addCategory(Intent.CATEGORY_BROWSABLE)
+            val resolvedIntent = intent?.takeIf {
+                requireContext().packageManager
+                    .resolveActivity(it, PackageManager.MATCH_DEFAULT_ONLY) != null
+            }
+            return resolvedIntent ?: intent?.let(::getFallbackIntent)
+        }
+
+        private fun getFallbackIntent(intent: Intent): Intent? {
+            return intent.getStringExtra("browser_fallback_url")
+                ?.let(Uri::parse)
+                ?.let { Intent(Intent.ACTION_VIEW, it) }
+                ?.apply { addCategory(Intent.CATEGORY_BROWSABLE) }
+                ?.takeIf {
+                    requireContext().packageManager
+                        .resolveActivity(it, PackageManager.MATCH_DEFAULT_ONLY) != null
+                }
         }
 
         private fun attemptHandleByExternalApp(uri: Uri): Boolean {
@@ -183,6 +246,8 @@ internal class WebViewFragment : Fragment() {
                     return true
                 } catch (_: Exception) {}
             }
+            // Don't intercept the load if an activity was not actually started;
+            // this allows the iframe or whatnot to get the error.
             return false
         }
 
@@ -191,7 +256,8 @@ internal class WebViewFragment : Fragment() {
                 getPackageNameForScheme(scheme)?.let(::setPackage)
                 addCategory(Intent.CATEGORY_BROWSABLE)
             }.takeIf {
-                val resolveInfo = requireContext().packageManager.resolveActivity(it, PackageManager.MATCH_DEFAULT_ONLY)
+                val resolveInfo = requireContext().packageManager
+                    .resolveActivity(it, PackageManager.MATCH_DEFAULT_ONLY)
                 resolveInfo != null && shouldStartActivity(uri, resolveInfo)
             }
         }
@@ -209,6 +275,45 @@ internal class WebViewFragment : Fragment() {
                     resolveInfo.match and IntentFilter.MATCH_CATEGORY_MASK >= IntentFilter.MATCH_CATEGORY_HOST
                 else -> true
             }
+        }
+    }
+
+    class ErrorDialogFragment : DialogFragment() {
+        companion object {
+            const val ARG_URI = "U"
+
+            fun newInstance(uri: Uri) = ErrorDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_URI, uri.toString())
+                }
+            }
+        }
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val context = requireContext()
+            return AlertDialog.Builder(context, theme)
+                .setTitle(R.string.swedbankpaysdk_intent_failure_title)
+                .setMessage(context.formatMessage())
+                .setNeutralButton(R.string.swedbankpaysdk_intent_failure_close) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+        }
+
+        private fun Context.formatMessage(): String {
+            val packageName = getFailingPackage() ?: getString(R.string.swedbankpaysdk_intent_failure_unknown)
+            return getString(R.string.swedbankpaysdk_intent_failure_body, packageName)
+        }
+
+
+        private fun getFailingPackage(): String? {
+            val uri = arguments?.getString(ARG_URI)
+            val intent = try {
+                uri?.let { Intent.parseUri(it, INTENT_URI_FLAGS) }
+            } catch (_: URISyntaxException) {
+                null
+            }
+            return intent?.`package`
         }
     }
 }
