4c40c03b7afc049082259747c02ba18a4d56eb54
Pertti Kroger <pertti@qvik.fi>
2020-05-14 15:00:10 +0300

    fix race condition in PaymentFragment completeUrl test

--- sdk/src/androidTest/java/com/swedbankpay/mobilesdk/test/fragment/PaymentFragmentShouldReportSuccessAfterNavigationToCompleteUrl.kt
+++ sdk/src/androidTest/java/com/swedbankpay/mobilesdk/test/fragment/PaymentFragmentShouldReportSuccessAfterNavigationToCompleteUrl.kt
@@ -2,6 +2,7 @@ package com.swedbankpay.mobilesdk.test.fragment
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Observer
+import androidx.test.espresso.web.assertion.WebViewAssertions
 import androidx.test.espresso.web.model.SimpleAtom
 import androidx.test.espresso.web.sugar.Web
 import com.nhaarman.mockitokotlin2.mock
@@ -35,6 +36,15 @@ class PaymentFragmentShouldReportSuccessAfterNavigationToCompleteUrl : BasePayme
 
         Web.onWebView()
             .perform(SimpleAtom("window.location = '${TestConstants.completeUrl}'"))
+            .check(WebViewAssertions.webContent(HasNoScriptsMatcher()))
+        // The webContent(HasNoScriptsMatcher()) assertion may seem unnecessary, harmful even,
+        // as we are not actually that interested in the WebView content at this point.
+        // However, it is needed for synchronization here. The .perform(SimpleAtom("window.location = foo"))
+        // call does not synchronize over loading of the new page, only the subsequent .check
+        // call will do that. This is the simplest way of preventing a race condition with
+        // scenario.onFragment below. Now, we could have our own Matcher here that matches
+        // any Document, but the no-scripts one will do also. If this test breaks in the future
+        // (and it really should not), then just change the matcher to a suitable one.
 
         scenario.onFragment {
             // Now, ideally we would verify here, but doing so will lose
