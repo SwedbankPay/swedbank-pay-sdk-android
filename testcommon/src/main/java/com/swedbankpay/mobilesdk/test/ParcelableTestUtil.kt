e2224baa8afc4b9c35b153c5a13e282d4c3b1821
Olof Thorén <olof.thoren@qvik.fi>
2022-08-24 14:59:38 +0200

    Postponing depricated methods

--- testcommon/src/main/java/com/swedbankpay/mobilesdk/test/ParcelableTestUtil.kt
+++ testcommon/src/main/java/com/swedbankpay/mobilesdk/test/ParcelableTestUtil.kt
@@ -2,17 +2,17 @@ package com.swedbankpay.mobilesdk.test
 
 import android.os.Parcel
 import android.os.Parcelable
-import androidx.annotation.RequiresApi
+//import androidx.annotation.RequiresApi
 import org.junit.Assert
 
-@RequiresApi(33)
+//@RequiresApi(33)
 fun testParcelable(parcelable: Parcelable) {
     val parcel = Parcel.obtain()
 
     parcel.writeParcelable(parcelable, 0)
     parcel.setDataPosition(0)
     
-    val unParseled = parcel.readParcelable<Parcelable>(parcelable.javaClass.classLoader, Parcelable::class.java)
+    val unParseled = parcel.readParcelable<Parcelable>(parcelable.javaClass.classLoader)    //, Parcelable::class.java
     Assert.assertEquals(parcelable, unParseled)
 
     parcel.recycle()
