b65455c9282bd67b287fb61936b38446661c9ab5
Olof Thorén <olof.thoren@qvik.fi>
2022-11-14 17:19:38 +0100

    Upgrading dependencies and preventing clashes with isTransitive = false

--- dependencies/dependencies.gradle.kts
+++ dependencies/dependencies.gradle.kts
@@ -12,7 +12,6 @@ extra["libs"] = mapOf(
    "androidx-appcompat" to "androidx.appcompat:appcompat:1.5.1",
    "androidx-core-ktx" to "androidx.core:core-ktx:1.9.0",
    "fragment-ktx" to "androidx.fragment:fragment-ktx:1.5.4",
-   "fragment-testing" to "androidx.fragment:fragment-testing:1.5.4",
    "lifecycle-livedata-ktx" to "androidx.lifecycle:lifecycle-livedata-ktx:2.5.1",
    "lifecycle-viewmodel-ktx" to "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1",
    "constraintlayout" to "androidx.constraintlayout:constraintlayout:2.1.4",
@@ -21,10 +20,10 @@ extra["libs"] = mapOf(
    "swiperefreshlayout" to "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0",
 
    "androidx-test-core" to "androidx.test:core:1.4.0",
-   "androidx-test-runner" to "androidx.test:runner:1.4.0",
-
-   "espresso-core" to "androidx.test.espresso:espresso-core:3.4.0",
-   "espresso-intents" to "androidx.test.espresso:espresso-intents:3.4.0",
+   "androidx-test-runner" to "androidx.test:runner:1.5.1",
+   
+   "espresso-core" to "androidx.test.espresso:espresso-core:3.5.0",
+   "espresso-intents" to "androidx.test.espresso:espresso-intents:3.5.0",
    "espresso-web" to "androidx.test.espresso:espresso-web:3.5.0",
 
    "androidx-test-junit" to "androidx.test.ext:junit:1.1.4",
@@ -46,5 +45,6 @@ extra["libs"] = mapOf(
    "robolectric" to "org.robolectric:robolectric:4.9",
    //"mockito-inline" to "org.mockito:mockito-inline:4.8.1",
    "mockito-kotlin" to "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0",
-   "dexmaker-mockito-inline" to "com.linkedin.dexmaker:dexmaker-mockito-inline:2.28.3"
+   "dexmaker-mockito-inline" to "com.linkedin.dexmaker:dexmaker-mockito-inline:2.28.3",
+   "fragment-testing" to "androidx.fragment:fragment-testing:1.5.4",
 )
