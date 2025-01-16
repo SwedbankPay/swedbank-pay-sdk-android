e088862613073134f47fba4d2faad5d8fe0822cf
Andreas Vettefors <andreas@appcorn.se>
2025-01-16 11:31:31 +0100

    Fix for Payment complete was sent to the merchant when it should not

--- mobilesdk/src/main/java/com/swedbankpay/mobilesdk/internal/PaymentFragmentStateBridge.kt
+++ mobilesdk/src/main/java/com/swedbankpay/mobilesdk/internal/PaymentFragmentStateBridge.kt
@@ -2,30 +2,13 @@ package com.swedbankpay.mobilesdk.internal
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.map
 import com.swedbankpay.mobilesdk.PaymentViewModel.State
-import com.swedbankpay.mobilesdk.internal.InternalPaymentViewModel.UIState
 
 
 /**
  * This object acts like a bridge between web based payments and the payment session
  */
 internal object PaymentFragmentStateBridge {
-
-    val paymentProcessState: MutableLiveData<UIState?> = MutableLiveData()
-    val paymentFragmentState: LiveData<State> = paymentProcessState.map {
-        when (it) {
-            null -> State.IDLE
-            UIState.Loading -> State.IN_PROGRESS
-            is UIState.ViewConsumerIdentification -> State.IN_PROGRESS
-            is UIState.ViewPaymentOrder -> State.IN_PROGRESS
-            is UIState.UpdatingPaymentOrder -> State.UPDATING_PAYMENT_ORDER
-            is UIState.InitializationError -> State.FAILURE
-            is UIState.RetryableError -> State.RETRYABLE_ERROR
-            UIState.Complete -> State.COMPLETE
-            UIState.Canceled -> State.CANCELED
-            is UIState.Failure -> State.FAILURE
-        }
-    }
-
+    val paymentProcessState: MutableLiveData<State?> = MutableLiveData()
+    val paymentFragmentState: LiveData<State?> = paymentProcessState
 }
