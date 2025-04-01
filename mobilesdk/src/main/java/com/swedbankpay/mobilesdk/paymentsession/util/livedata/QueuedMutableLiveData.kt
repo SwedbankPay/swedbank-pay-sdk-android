e3ea40e5229b244ef04d5b0dc58e265a1a46af96
Andreas Vettefors <andreas@appcorn.se>
2025-04-01 13:27:07 +0200

    Fix for observeAsState didn't get all values from queued live data

--- mobilesdk/src/main/java/com/swedbankpay/mobilesdk/paymentsession/util/livedata/QueuedMutableLiveData.kt
+++ mobilesdk/src/main/java/com/swedbankpay/mobilesdk/paymentsession/util/livedata/QueuedMutableLiveData.kt
@@ -1,6 +1,11 @@
 package com.swedbankpay.mobilesdk.paymentsession.util.livedata
 
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.*
 
 /**
@@ -9,14 +14,21 @@ import java.util.*
  */
 class QueuedMutableLiveData<T>(value: T) : MutableLiveData<T>(value) {
 
+    private val scope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+
     private val values: Queue<T> = LinkedList()
 
     private var isActive: Boolean = false
 
     override fun onActive() {
         isActive = true
-        while (values.isNotEmpty()) {
-            values.poll()?.let { setValue(it) }
+        scope.launch {
+            while (values.isNotEmpty()) {
+                values.poll()?.let {
+                    setValue(it)
+                    delay(100)
+                }
+            }
         }
     }
 
